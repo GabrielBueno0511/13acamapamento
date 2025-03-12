1a245357d86ed770efaa8fb58b7929b81736f80e
Gabriel Bueno <j.brielbueno05@gmail.com>
2025-03-12 09:32:31 -0300

    fix: footer css

--- src/pages/Home/sections/Footer/Footer.tsx
+++ src/pages/Home/sections/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
       <footer className="footer">
         <div className="footer-content">
-          <p>&copy; 2024 Acampamento. Todos os direitos reservados.</p>
+          <p>&copy; 2025 Acampamento. Todos os direitos reservados.</p>
           <nav>
             <span>Contato: bueno.dev@outlook.com</span>
           </nav>
